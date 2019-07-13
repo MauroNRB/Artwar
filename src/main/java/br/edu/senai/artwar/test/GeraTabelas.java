02cccc2636610b7952141573afd028f399d93786
MauroRibeiro <37764309+MauroNRB@users.noreply.github.com>
2019-07-13 13:28:20 -0300

    Add files via upload

---
+++ src/main/java/br/edu/senai/artwar/test/GeraTabelas.java
@@ -0,0 +1,11 @@
+package br.edu.senai.artwar.test;
+
+import br.edu.senai.artwar.util.Conexao;
+
+public class GeraTabelas {
+
+    public static void main(String[] args) {
+        Conexao.getEM();
+    }
+    
+}
